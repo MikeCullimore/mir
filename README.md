228b55a0e86ad6ab6724f887e71d3fadebe81a0b
Mike Cullimore <michael.cullimore11@gmail.com>
2023-01-07 19:27:58 +0000

    Try to identify note onsets via NMF.

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ pip install -r requirements.txtpip
 
 ## todo
 
+* Analyse keyboard intro to Four out of Five:
+    * Time 4-16s
+    * Apply chord recognition from FMP p256.
+    * Infer tuning (centre frequency) and align chroma on that.
+    * Extract melody: see FMP chapter 8 and https://www.audiolabs-erlangen.de/resources/MIR/FMP/C8/C8S2_MelodyExtractSep.html
+    * Nonnegative matrix factorisation (NMF) -based audio decomposition: template matrix of MIDI note numbers and their frequencies (fundamental and harmonics) and activation matrix (note numbers vs time).
 * Save plots to file.
 * Analyse songs in data folder:
     * Chromagram
@@ -23,3 +29,10 @@ pip install -r requirements.txtpip
     * Separate harmonic and percussive, save to files, listen (and adjust params if need be).
 * Add songs to data folder: Beatles, White Stripes.
 * Get PySoundFile working for load?
+* Use [Viterbi](https://librosa.org/doc/main/auto_examples/plot_viterbi.html#sphx-glr-auto-examples-plot-viterbi-py) to truncate silence at beginning and end (only).
+* Use [Laplacian segmentation](https://librosa.org/doc/main/auto_examples/plot_segmentation.html#sphx-glr-auto-examples-plot-segmentation-py) to identify and label song sections (verse, chorus etc.).
+* Play audio with vertical line sweeping through plot (e.g. chroma) in sync.
+
+## References
+
+* FMP: Fundamentals of Music Processing by Meinard Mueller. See [website](https://www.audiolabs-erlangen.de/resources/MIR/FMP/C0/C0.html).
--- main.py
+++ main.py
@@ -5,41 +5,79 @@ import numpy as np
 
 def main():
     # todo: replace with regex search for all files in data folder.
-    filepaths = ['data/03_automatic_stop.mp3']
+    # filepaths = ['data/03_automatic_stop.mp3']
+    filepaths = ['data/Four Out Of Five.wav'] # todo: analyse 4-16s piano intro, chord detection. Chord or melody?
     for filepath in filepaths:
         analyse_audio(filepath)
 
 def analyse_audio(filepath):
     # Load audio from file (snippet).
-    y, sr = librosa.load(filepath, duration=8.7)
+    y, sr = librosa.load(filepath, duration=16)
 
-    # Plot waveform
-    fig, ax = plt.subplots()
-    librosa.display.waveshow(y, sr=sr, ax=ax)
-    plt.show()
+    # Estimate tempo.
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
-    # todo: harmonic/percussive separation.
-    # harmonic, percussive = librosa.decompose.hpss()
-    y_harm = librosa.effects.harmonic(y=y, margin=8)
-    chroma_harm = librosa.feature.chroma_cqt(y=y_harm, sr=sr)
-    chroma_cqt = librosa.feature.chroma_cqt(y=y, sr=sr)
+    # For display purposes, let's zoom in on a 15-second chunk from the middle of the song
+    # https://librosa.org/doc/main/auto_examples/plot_chroma.html
+    # todo: wrap as method.
+    # todo: how to truncate original signal rather than chroma?
+    start = 4
+    # finish = start + 1.5
+    finish = start + 5
+    # finish = 16
+    idx = tuple([slice(None), slice(*list(librosa.time_to_frames([start, finish])))])
 
-    # todo: filter following this page: https://librosa.org/doc/main/auto_examples/plot_chroma.html
-    fig, ax = plt.subplots()
+    # # Plot waveform
+    # fig, ax = plt.subplots()
+    # librosa.display.waveshow(y, sr=sr, ax=ax)
+    # plt.show()
+
+    # # todo: harmonic/percussive separation.
+    # # harmonic, percussive = librosa.decompose.hpss()
+    # y_harm = librosa.effects.harmonic(y=y, margin=8)
+    # chroma_harm = librosa.feature.chroma_cqt(y=y_harm, sr=sr)[idx]
+    # chroma_cqt = librosa.feature.chroma_cqt(y=y, sr=sr)[idx]
+
+    # # Plot chroma.
+    # # todo: filter following this page: https://librosa.org/doc/main/auto_examples/plot_chroma.html
+    # fig, ax = plt.subplots()
     # img = librosa.display.specshow(chroma_cqt, y_axis='chroma', x_axis='time', ax=ax)
-    img = librosa.display.specshow(chroma_harm, y_axis='chroma', x_axis='time', ax=ax)
-    ax.set_title('Chroma CQT')
-    fig.colorbar(img, ax=ax)
-    plt.show()
+    # # img = librosa.display.specshow(chroma_harm, y_axis='chroma', x_axis='time', ax=ax)
+    # ax.set_title('Chroma CQT')
+    # fig.colorbar(img, ax=ax)
+    # plt.show()
 
-    # todo: combine with plot above.
-    C = np.abs(librosa.cqt(y=y, sr=sr, bins_per_octave=12*3, n_bins=7*12*3))
-    fig, ax = plt.subplots()
-    img = librosa.display.specshow(librosa.amplitude_to_db(C, ref=np.max),
-                                   y_axis='cqt_note', x_axis='time', bins_per_octave=12*3,
-                                   ax=ax)
-    fig.colorbar(img, ax=[ax], format="%+2.f dB")
-    ax.label_outer()
+    # # Plot chroma to high frequency.
+    # # todo: combine with plot above.
+    # # todo: drop bins below C3.
+    # C = np.abs(librosa.cqt(y=y, sr=sr, bins_per_octave=12*3, n_bins=7*12*3))
+    # data = librosa.amplitude_to_db(C, ref=np.max)[idx]
+    # fig, ax = plt.subplots()
+    # img = librosa.display.specshow(data,
+    #                                y_axis='cqt_note', x_axis='time', bins_per_octave=12*3,
+    #                                ax=ax)
+    # fig.colorbar(img, ax=[ax], format="%+2.f dB")
+    # ax.label_outer()
+    # plt.show()
+
+    # NMF
+    # todo: configure so that the components are equal temperament, A4 = 440Hz.
+    # todo: shift tuning (A4) to optimise template matching. But are glitchy activations down to vibrato?
+    # todo: axis labels (note scientific names).
+    # for n_steps in np.linspace(-1, 1, 7):
+        # y_shift = librosa.effects.pitch_shift(y, sr=sr, n_steps=-0.1) # todo: optimise n_steps for clean activations.
+    # S = np.abs(librosa.stft(y))[idx]
+    S = np.abs(librosa.cqt(y=y, sr=sr, bins_per_octave=12*3, n_bins=7*12*3))[idx]
+    comps, acts = librosa.decompose.decompose(S, n_components=16, sort=True)
+    fig, ax = plt.subplots(1, 2)
+    librosa.display.specshow(librosa.amplitude_to_db(comps, ref=np.max), ax=ax[0], y_axis='cqt_note')
+    librosa.display.specshow(acts, ax=ax[1], cmap='gray_r', x_axis='time')
+    ax[0].set_title('Components')
+    # ax[0].set_xlabel('Note')
+    # ax[0].set_ylabel('Frequency [Hz]')
+    ax[1].set_title('Activations')
+    fig.suptitle('Identiyfing note activations via NMF. Input: Four out of Five')
     plt.show()
 
 if __name__ == '__main__':
